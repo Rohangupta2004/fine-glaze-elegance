12d69f3669cf3b79394441c837d658d432f128c5
Rohangupta2004 <127654556+Rohangupta2004@users.noreply.github.com>
2026-01-22 00:18:56 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
-
+import { createClient } from "../lib/supabase"; 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL!,
   import.meta.env.VITE_SUPABASE_ANON_KEY!
